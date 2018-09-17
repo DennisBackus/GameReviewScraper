92b8d143e039e01f51a9a027e2fb15835cc1154b
danvandeee <danvandeee@hotmail.com>
2018-09-17 11:18:36 +0200

    check of de gezochte gametitle niet al in de database staat setup v1

---
+++ src/main/java/com/reviewscraper/games/service/IScrapeService.java
@@ -0,0 +1,23 @@
+package com.reviewscraper.games.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.reviewscraper.games.dao.IReviewDAO;
+import com.reviewscraper.games.models.Game;
+import com.reviewscraper.games.models.Review;
+
+@Service
+public interface IScrapeService {
+
+
+	List<Review> findByGame(Game game);
+	
+	
+	
+	
+	
+	
+}
