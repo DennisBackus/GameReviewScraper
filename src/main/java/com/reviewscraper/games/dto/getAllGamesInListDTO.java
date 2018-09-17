0d41b9820dc14b1a6aeb643eb26bd6773f39c168
danvandeee <danvandeee@hotmail.com>
2018-09-17 10:18:59 +0200

    eerste commit, hierbij is een DTO aangemaakt en is de functie: get all games, toegevoegd

---
+++ src/main/java/com/reviewscraper/games/dto/getAllGamesInListDTO.java
@@ -0,0 +1,45 @@
+package com.reviewscraper.games.dto;
+
+import java.util.List;
+
+import javax.persistence.OneToMany;
+
+import com.reviewscraper.games.models.Review;
+
+public class getAllGamesInListDTO {
+
+	private Long id;
+
+	private String gameTitle;
+	private String gameStudio;
+	private String releaseDate;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getGameTitle() {
+		return gameTitle;
+	}
+	public void setGameTitle(String gameTitle) {
+		this.gameTitle = gameTitle;
+	}
+	public String getGameStudio() {
+		return gameStudio;
+	}
+	public void setGameStudio(String gameStudio) {
+		this.gameStudio = gameStudio;
+	}
+	public String getReleaseDate() {
+		return releaseDate;
+	}
+	public void setReleaseDate(String releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+	
+	//private List<Review> reviews;
+	
+	
+	
+}
